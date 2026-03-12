e49ddccc9ffa31caf85b014812a06dec48e8f4f4
webOwl <nikitaterentev916@gmail.com>
2026-03-12 17:40:23 +0100

    feat(products-section): add adaptive styles

--- ArcticOwl/src/components/products/ProductsSection.jsx
+++ ArcticOwl/src/components/products/ProductsSection.jsx
@@ -22,7 +22,6 @@ export const ProductsSection = forwardRef(function ProductsSection(
       const response = await axios.get("http://localhost:3001/products");
       setProducts(response.data);
     };
-
     getProducts();
   }, []);
 
@@ -36,51 +35,49 @@ export const ProductsSection = forwardRef(function ProductsSection(
             </div>
           ) : (
             <ul className="products-section__list">
-              {filteredProducts.map((product) => {
-                return (
-                  <li key={product.id} className="products-section__item">
-                    <article className="products-section__product product">
-                      <Link to={`/product-details/${product.id}`}>
+              {filteredProducts.map((product) => (
+                <li key={product.id} className="products-section__item">
+                  <article className="products-section__product product">
+                    <Link to={`/product-details/${product.id}`}>
+                      <div className="product__img-wrapper">
                         <img
                           loading="lazy"
                           className="product__img"
                           src={product.image || "../../images/no-image.png"}
                           alt="product image"
                         />
-                        <h3 className="product__title">{product.name}</h3>
-                      </Link>
-
-                      <div className="product__footer">
-                        <div className="product__price-container">
-                          {product.salePrice ? (
-                            <>
-                              <s className="product__old-price">
-                                ${product.price}
-                              </s>
-                              <span className="product__sale-price">
-                                ${product.salePrice}
-                              </span>
-                            </>
-                          ) : (
-                            <span className="product__usual-price">
-                              ${product.price}
-                            </span>
-                          )}
-                        </div>
-
-                        <button
-                          className="product__add-btn"
-                          onClick={() => addToCart(product)}
-                        >
-                          <svg className="product__cart-icon">
-                            <use href="/symbol-defs.svg#icon-cart"></use>
-                          </svg>
-                        </button>
                       </div>
-                    </article>
-                  </li>
-                );
-              })}
+                      <h3 className="product__title">{product.name}</h3>
+                    </Link>
+                    <div className="product__footer">
+                      <div className="product__price-container">
+                        {product.salePrice ? (
+                          <>
+                            <s className="product__old-price">
+                              ${product.price}
+                            </s>
+                            <span className="product__sale-price">
+                              ${product.salePrice}
+                            </span>
+                          </>
+                        ) : (
+                          <span className="product__usual-price">
+                            ${product.price}
+                          </span>
+                        )}
+                      </div>
+                      <button
+                        className="product__add-btn"
+                        onClick={() => addToCart(product)}
+                      >
+                        <svg className="product__cart-icon">
+                          <use href="/symbol-defs.svg#icon-cart"></use>
+                        </svg>
+                      </button>
+                    </div>
+                  </article>
+                </li>
+              ))}
             </ul>
           )}
         </div>
