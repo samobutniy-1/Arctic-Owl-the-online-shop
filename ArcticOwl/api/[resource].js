e56f3da89021944d59e84ab08c87fc9185099882
webOwl <nikitaterentev916@gmail.com>
2026-04-14 12:02:44 +0200

    debug: check api contents

--- ArcticOwl/api/[resource].js
+++ ArcticOwl/api/[resource].js
@@ -1,10 +1,9 @@
 import { readdirSync } from "fs";
-import { join } from "path";
 
 export default function handler(req, res) {
   try {
-    const api = readdirSync(join(process.cwd(), "ArcticOwl/api"));
-    res.json({ api });
+    const root = readdirSync("/var/task/ArcticOwl/api");
+    res.json({ root });
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
